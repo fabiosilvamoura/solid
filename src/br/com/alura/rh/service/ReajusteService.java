804fddc5ba0ddada66387bd09f52a26cda684ad9
fabio moura <fmsystem@gmail.com>
2022-09-08 20:57:18 -0300

    primeiro commit

---
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,30 @@
+package br.com.alura.rh.service;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class ReajusteService {
+
+    public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser maior que 6 meses");
+        }
+
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.reajustarSalario(salarioReajustado);
+    }
+}
